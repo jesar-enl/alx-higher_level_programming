cf9ab0a67fcbde063e15a68391204646a6f0678a
Jesse Kyambadde <85540701+jesar-enl@users.noreply.github.com>
2022-11-29 20:31:58 +0300

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,9 +2,9 @@
 '''
 Program to print numbers from 00 t0 99
 '''
-for i in range(0,100):
+for i in range(100):
     if i == 99:
-        print("{:02d}".format(i), end=" ")
+        print("{:02d}".format(i,))
     else:
-        print("{:02d},".format(i), end=" ")
+        print("{:02d}".format(i,), end=", ")
 
