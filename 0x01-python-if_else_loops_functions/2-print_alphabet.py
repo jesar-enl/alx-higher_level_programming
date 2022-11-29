6fdd4f1487a0ff6bec448c5bb5a8247dc8708127
Jesse Kyambadde <85540701+jesar-enl@users.noreply.github.com>
2022-11-29 20:10:35 +0300

    Edited string format

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 # Print the alphabet in lowercase, not followed by a newline
 for alpha in range(ord('a'), ord('z') + 1):
-    print("{:s}.format(chr(alpha))", end="")
+    print("{:s}".format(chr(alpha)), end="")
