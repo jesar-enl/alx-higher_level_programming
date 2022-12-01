70b242be74b7c89f110bc310b70124f40a6511c7
Jesse Kyambadde <85540701+jesar-enl@users.noreply.github.com>
2022-12-01 12:12:14 +0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+if __name__ == '__main__':
     from sys import argv
-
-# get the number of elements of argv
 nargs = len(argv)
-
-# print the number of elements/ arguments
 if nargs == 1:
-    # when no arguments are provided while executing
     print("{:d} arguments.".format(nargs - 1))
 elif nargs > 1:
-    # if arguments are provided.
-    msg = "argument" if (nargs == 2) else "argments"
-    print("{:d} {:s}:".format(nargs - 1, msg))
-    # also print the arguments thatare given starting from index[1]
-    for a in range(1, nargs):
-        print("{:d}: {:s}".format(a, argv[a]))
+    message = "argument" if (nargs == 2) else "arguments"
+    print("{:d} {:s}:".format(nargs - 1, message))
+    for i in range(1, nargs):
+        print("{:d}: {:s}".format(i, argv[i]))
