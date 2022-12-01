ca023cddd8108eb0df13894ec933862aa71d744a
Jesse Kyambadde <jessekyambadde0@gmail.com>
2022-12-01 10:57:36 +0300

    modules_import: add the arguments provided

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+
+# get the length of the arguments
+n = len(argv)
+
+# get the sum of the arguments passed
+sum = 0
+for i  in range(1, n):
+    sum += int(argv[i])
+
+# print the result
+print("{:d}".format(sum))
