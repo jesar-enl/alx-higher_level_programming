e63a033b03695f61abc108b4be33fb1bda5456c3
Jesse Kyambadde <jessekyambadde0@gmail.com>
2022-12-04 13:19:22 +0300

    Remove c and C in a string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,9 +3,9 @@ def no_c(my_string):
     if my_string is not None:
         my_list = list(my_string)
         new_list = []
-        for l in my_list:
-            if l != 'c' and l != 'C':
-                new_list.append(l)
+        for letter in my_list:
+            if letter != 'c' and letter != 'C':
+                new_list.append(letter)
         new_string = ''.join(new_list)
         return new_string
     else:
