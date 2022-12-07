9ab32c5a1a008009674eb8d1f22261d789e64ab0
Jesse Kyambadde <jessekyambadde0@gmail.com>
2022-12-07 18:46:27 +0300

    Search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/pyhton3
 
 def search_replace(my_list, search, replace):
-    new = []
-    new = list(map(lambda n: n if (n is not search) else replace, my_list))
-    return new
+    newl = []
+    newl = list(map(lambda n: n if (n is not search) else replace, my_list))
+    return newl
