736c63aaef4ec3f01b52e18f63c625238e6a0296
Jesse Kyambadde <jessekyambadde0@gmail.com>
2023-01-09 14:33:06 +0300

    Inheritance

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module containing ``Square`` class inheriting from
+``Rectangle`` class
+"""
+
+
+base_g = __import__('9-rectangle')
+
+
+class Square(base_g.Rectangle):
+    """Class definition"""
+    def __init__(self, size):
+        """Initialize the square attributes"""
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Compute the area of the square"""
+        return self.__size ** 2
