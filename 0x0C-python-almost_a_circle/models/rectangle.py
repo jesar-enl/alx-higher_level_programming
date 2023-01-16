44e995f2b5961298afd034292a99c1b7098affe0
Jesse Kyambadde <jessekyambadde0@gmail.com>
2023-01-16 19:12:27 +0300

    edited

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -94,8 +94,8 @@ class Rectangle(Base):
 
     def __str__(self):
         """String representation"""
-        return f"[Rectangle] ({self.id}) \
-                {self.x}/{self.y} - {self.width}/{self.height}"
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+                self.id, self.x, self.y, self.width, self.height)
 
     def update(self, *args, **kwargs):
         """update the argument placement"""
