9cae0e78acc28b44bc61dec7debdbf51f7eec003
Jesse Kyambadde <jessekyambadde0@gmail.com>
2023-03-31 12:02:28 +0300

    Display the HTTP methods used

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# print all the HTTP request methods allewd by server
-curl -sIX OPTIONS "$1" | grep -i "Allow:" | awk '{print $2}'
+# print all the HTTP request methods allowed by server
+curl -sI "$1" | grep -i "Allow:" | awk '{print $2}' | xargs
