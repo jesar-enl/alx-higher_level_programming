1f01d71c5e3544e6020611439f2509be3b443bd8
Jesse Kyambadde <jessekyambadde0@gmail.com>
2023-04-04 18:29:30 +0300

    My GitHub

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,18 +9,13 @@ import sys
 
 
 if __name__ == "__main__":
-    # Define the GitHub API endpoint
-    url = 'https://api.github.com/user'
-
-    # Set the username and personal access token
     username = sys.argv[1]
     password = sys.argv[2]
-
-    # Set the headers with the Basic Authentication credentials
-    headers = {'Authorization': f'Basic {username}:{password}'}
-
-    # Make the API request to get the user ID
-    response = requests.get(url, headers=headers)
-
-    # Print the user ID
-    print(response.json().get('id'))
+    url = ' https://api.github.com/users/{}'.format(username)
+    headers = {'Authorization': 'Bearer {}'.format(password)}
+    try:
+        r = requests.get(url, headers=headers)
+        response = r.json()
+        print(response.get('id'))
+    except (ValueError, TypeError) as err:
+        pass
